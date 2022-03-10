0140f4779a46ffadc95774c904eb1183f70b6004
Fábio Monari <fabiomonari@gmail.com>
2022-03-10 08:55:57 -0300

    Removing Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { lastValueFrom } from 'rxjs';
 
 @Component({
@@ -22,31 +21,28 @@ export class AppComponent implements OnInit {
   selectedRegion = 'all';
   searchText = '';
 
-  constructor(
-    private _httpClient: HttpClient,
-    private _snackBar: MatSnackBar
-  ) {}
+  constructor(private _httpClient: HttpClient) {}
 
   ngOnInit() {
     this.getCountries();
   }
 
   async getCountries(): Promise<any> {
-    await lastValueFrom(this._httpClient.get(this.API_URL))
-      .then((data) => {
-        this.countries = data;
-      })
-      .catch((err) => {
-        this._snackBar.open('Erro loading data', 'OK', {
-          verticalPosition: 'top',
-          horizontalPosition: 'right',
-          panelClass: 'snackbar-error',
-        });
-        console.log(
-          '%cPromise rejected with',
-          'background-color: red; color: white; border-radius: 2px; padding: 2px',
-          JSON.stringify(err)
-        );
-      });
+    await lastValueFrom(this._httpClient.get(this.API_URL)).then((data) => {
+      this.countries = data;
+    });
+
+    // .catch((err) => {
+    //   this._snackBar.open('Erro loading data', 'OK', {
+    //     verticalPosition: 'top',
+    //     horizontalPosition: 'right',
+    //     panelClass: 'snackbar-error',
+    //   });
+    //   console.log(
+    //     '%cPromise rejected with',
+    //     'background-color: red; color: white; border-radius: 2px; padding: 2px',
+    //     JSON.stringify(err)
+    //   );
+    // });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatCardModule } from '@angular/material/card';
 import { SearchFilterPipe } from './pipes/search-filter.pipe';
 import { FormsModule } from '@angular/forms';
 
@@ -25,15 +16,6 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    MatIconModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatToolbarModule,
-    MatProgressBarModule,
-    MatSnackBarModule,
-    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
