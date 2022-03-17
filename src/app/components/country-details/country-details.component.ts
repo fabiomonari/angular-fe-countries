d090744edf43f5cfa704e525316be8de3a5fd5ed
Fábio Monari <fabiomonari@gmail.com>
2022-03-17 18:21:02 -0300

    Adding country-details component

--- src/app/components/country-details/country-details.component.ts
+++ src/app/components/country-details/country-details.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-country-details',
   templateUrl: './country-details.component.html',
-  styleUrls: ['./country-details.component.scss']
+  styleUrls: ['./country-details.component.scss'],
 })
 export class CountryDetailsComponent implements OnInit {
+  country: any = [];
+  isReady = false;
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.httpService
+      .getCountry(this.route.snapshot.params['id'])
+      .then((data) => {
+        this.country = data;
+        this.isReady = true;
+      });
   }
-
 }
